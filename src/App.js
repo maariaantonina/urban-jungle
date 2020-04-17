7b93780ac32110f553f4c070eac93348089b1d3b
Maria Kasińska <mariakasinska17@gmail.com>
2020-04-17 13:13:45 +0200

    add elements to Product Component

--- src/App.js
+++ src/App.js
@@ -37,6 +37,8 @@ const theme = createMuiTheme({
     },
     body1: {
       fontFamily: ['Noto Serif', 'serif'].join(','),
+      fontSize: '0.875REM',
+      lineHeight: 2,
     },
     body2: {
       fontFamily: ['Inter', 'sans-serif'].join(','),
