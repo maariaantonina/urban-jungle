dab7072074ed9f5a1ba49cf8a39176e5b73aafa7
Maria Kasińska <mariakasinska17@gmail.com>
2020-05-02 12:45:38 +0200

    change manifest.json

--- backend/server.js
+++ backend/server.js
@@ -12,6 +12,8 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
+app.use(express.static(path.join(__dirname + '../public')));
+
 /* MONGOOSE */
 mongoose.connect('mongodb://localhost:27017/urban-jungle', {
   useNewUrlParser: true,
